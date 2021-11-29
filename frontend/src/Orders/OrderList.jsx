f62af60ae9fd011eccf5dcff49b5b8ba3a737db7
ashrajesh <ashwinr@smu.edu>
2021-11-28 18:31:33 -0600

    order table configured

---
+++ frontend/src/Orders/OrderList.jsx
@@ -0,0 +1,48 @@
+import { Link } from 'react-router-dom';
+
+export const OrderList = props => {
+    if (!props.orders) {
+        return <div>Loading...</div>
+    }
+       return <div className="container">
+                <table className="table table-striped">
+                    <thead>
+                        <tr>
+                        <th scope="col">foodDonationID</th>
+                        <th scope="col">Soup Kitchen</th>
+                        <th scope="col">driverID</th>
+                        <th scope="col">foodName</th>
+                        <th scope="col">timeMade</th>
+                        </tr>
+                    </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>North Texas Food Bank</td>
+                                <td>25</td>
+                                <td>Mac n Cheese</td>
+                                <td>11/9/2021 2:16 PM</td>
+                            </tr>
+                            {
+                                props.accounts.map(order => <tr key={order.foodDonationID}>
+                                    <td>{order.soupKitchen}</td>
+                                    <td>{order.driverID}</td>
+                                    <td>{order.foodName}</td>
+                                    <td>{order.timeMade}</td>
+                                    {/* <td>
+                                        <Link to={`edit/${order.food}`}>{account.name}</Link>
+                                    </td>
+                                    <td>{account.email}</td>
+                                    <td>{account.isEmployee ? 'YES' : 'NO'}</td>
+                                    <td>
+                                        <button type="button"
+                                            className="btn btn-sm btn-danger"
+                                            onClick={() => props.onAccountDelete(account.id)}>
+                                            X</button>
+                                    </td> */}
+                                </tr>)
+                            }
+                        </tbody>
+                </table>
+            </div>
+            };
