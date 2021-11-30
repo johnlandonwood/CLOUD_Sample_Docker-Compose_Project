02455ce40848e436a2d78b27ae498cfae395f95d
Landon Wood <landonw@smu.edu>
2021-11-30 17:40:14 -0600

    Update config.js

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -1,2 +1,3 @@
-export const hostname = "http://localhost:8000";
+// export const hostname = "http://localhost:8000";
+export const hostname = "http://ec2-3-12-196-154.us-east-2.compute.amazonaws.com:8000";
 export default hostname;
